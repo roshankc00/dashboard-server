9a2489158b8c9b11e1140230a109d82c92b649c7
Roshan karki <rohitkc8848@gmail.com>
2026-05-16 13:52:53 +0545

    feat:tested the jest working as expected

--- src/lib/sum.test.ts
+++ src/lib/sum.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from "@jest/globals";
 
+// This is a test file for the sum function
 describe("basic math test", () => {
     it("2 + 2 should equal 4", () => {
         expect(2 + 2).toBe(4);
